f0a5de8efbb2ce0a5ff4edaf597684c18372ee4b
Joaquim A. Muniz Leite <munizleiteja@gmail.com>
2026-02-27 22:35:09 +0000

    Criação do Fluxo de Cadastro e Atualização do Perfil Médico

--- life_alert/src/life_alert/usuarios/UsuarioCivil.py
+++ life_alert/src/life_alert/usuarios/UsuarioCivil.py
@@ -3,11 +3,13 @@ from .Usuario import Usuario
 class Civil(Usuario):
     def __init__(self, nome, cpf, telefone, rua, num, bairro, cidade, estado, email, senha):
         super().__init__(nome, cpf, telefone, rua, num, bairro, cidade, estado, email, senha, tipo="Civil")
+        self.perfil_medico = None
     
     def exibirMenu(self):
         super().exibirMenu()
         print("3 - Registrar Ocorrência")
         print("4 - Acompanhar minhas Ocorrências")
+        print("5 - Gerenciar Perfil Médico")
 
     def registrarOcorrencia():
         pass
