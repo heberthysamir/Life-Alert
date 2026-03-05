d34c95da09dd1998b2f0c44a7b138bc4166c97e5
heberthysamir <heberthy.samir@aluno.ufca.edu.br>
2026-03-05 14:25:01 -0300

    Interface-Inicial

--- life_alert/src/life_alert/domain/usuarios/UsuarioCivil.py
+++ life_alert/src/life_alert/domain/usuarios/UsuarioCivil.py
@@ -5,11 +5,11 @@ class Civil(Usuario):
         super().__init__(nome, cpf, telefone, rua, num, bairro, cidade, estado, email, senha, tipo="Civil")
         self.perfil_medico = None
     
-    def exibirMenu(self):
-        super().exibirMenu()
-        print("3 - Registrar Ocorrência")
-        print("4 - Acompanhar minhas Ocorrências")
-        print("5 - Gerenciar Perfil Médico")
-
-    def registrarOcorrencia():
-        pass
+    def obter_funcionalidades(self):
+        acoes = super().obter_funcionalidades() # Pega as ações de atualizar/excluir
+        acoes.extend([
+            ("🚨 Nova Ocorrência", lambda container: self.gui_ref.tela_criar_ocorrencia(container)),
+            ("📋 Minhas Ocorrências", lambda container: self.gui_ref.tela_listar_ocorrencias(container)),
+            ("🏥 Perfil Médico", lambda container: self.gui_ref.tela_perfil_medico(container))
+        ])
+        return acoes
